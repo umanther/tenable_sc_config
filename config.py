f7159f9e44bb93b42668a9fee6eb8921e9ee823e
Geoffrey Hellstrand <ubil@hotmail.com>
2019-11-01 12:29:04 -0700

    Made various changes Updated documentation Created default file name constant
    Signed-off-by: Geoffrey Hellstrand <ubil@hotmail.com>

--- config.py
+++ config.py
@@ -6,30 +6,34 @@ configuration files used in SecurityCenter programs.
 This file can also be run standalone to generate an example configuration file.
 
 functions:
-
-    create_blank_config_file() -> configparser.ConfigParser:
+    create_new() -> configparser.ConfigParser:
         Generate and return an example configuration file object
         as a ConfigParser object
 
-    validate_config_file(file: str = 'config.ini') -> (configparser.ConfigParser, Exception):
+    validate(file: str = DEFAULT_FILE_NAME) -> (configparser.ConfigParser, Exception):
         Reads and validates the file provided to be valid config, containing all required sections.
         Returns a ConfigParser object and any exception errors
 
-    read_config_file(file: str = 'config.ini') -> configparser.ConfigParser:
+    read(file: str = DEFAULT_FILE_NAME) -> configparser.ConfigParser:
         Reads provided file, returning a ConfigParser object.
 
-    save_config_file(config: configparser.ConfigParser, file: str = 'config.ini'):
+    save(config: configparser.ConfigParser, file: str = DEFAULT_FILE_NAME):
         Writes the provided ConfigParser object to a file.
 
-error class:
+constants:
+    DEFAULT_FILE_NAME
+        Used as a default name for all functions.
+
+error classes:
     InvalidConfigurationFile
         Raised for a failed configuration file validation
-
 """
 from configparser import ConfigParser
 
-__all__ = ["create_new", "read", "save", "validate",
-           "UnableToCreateFile", "InvalidConfigurationFile"]
+DEFAULT_FILE_NAME = 'TenableSCConfig.ini'
+
+__all__ = ['create_new', 'read', 'save', 'validate',
+           'UnableToCreateFile', 'InvalidConfigurationFile', 'DEFAULT_FILE_NAME']
 
 
 def create_new() -> ConfigParser:
@@ -51,22 +55,22 @@ def create_new() -> ConfigParser:
     return config
 
 
-def save(config: ConfigParser, file: str = 'config.ini'):
+def save(config: ConfigParser, file: str = DEFAULT_FILE_NAME):
     """Saves passed ConfigParser object to file location"""
     if not isinstance(config, ConfigParser):
-        raise TypeError("ConfigParser type not passed to save_config_file")
+        raise TypeError('ConfigParser type not passed to save_config_file')
 
     try:
         with open(file, 'w') as config_file:
             config.write(config_file)
-    except OSError as err:
-        raise OSError("File Error: {0}".format(err))
-    except TypeError as err:
-        raise TypeError("Invalid Parameter: {0}".format(err))
+    except OSError as e:
+        raise OSError(f'File Error: {e}')
+    except TypeError as e:
+        raise TypeError(f'Invalid Parameter: {e}')
 
 
-def read(file: str = 'config.ini') -> ConfigParser:
-    """Attempts to read passed config.ini file and return the results in a ConfigParser object"""
+def read(file: str = DEFAULT_FILE_NAME) -> ConfigParser:
+    """Attempts to read passed file and return the results in a ConfigParser object"""
     config = ConfigParser()
 
     try:
@@ -77,7 +81,7 @@ def read(file: str = 'config.ini') -> ConfigParser:
     return config
 
 
-def validate(file: str = 'config.ini') -> (ConfigParser, Exception):
+def validate(file: str = DEFAULT_FILE_NAME) -> (ConfigParser, Exception):
     config = read(file)
 
     config_to_lower = {}
@@ -85,17 +89,17 @@ def validate(file: str = 'config.ini') -> (ConfigParser, Exception):
         config_to_lower[items.lower()] = items
 
     if 'securitycenter' not in config_to_lower:
-        return None, InvalidConfigurationFile("[SecurityCenter] section not found")
+        return None, InvalidConfigurationFile('[SecurityCenter] section not found')
     if 'user' not in config_to_lower:
-        return None, InvalidConfigurationFile("[User] section not found")
+        return None, InvalidConfigurationFile('[User] section not found')
 
     if 'hostname' not in config[config_to_lower['securitycenter']]:
-        return None, InvalidConfigurationFile("hostname parameter not found")
+        return None, InvalidConfigurationFile('hostname parameter not found')
 
     if 'username' not in config[config_to_lower['user']]:
-        return None, InvalidConfigurationFile("username parameter not found")
+        return None, InvalidConfigurationFile('username parameter not found')
     if 'password' not in config[config_to_lower['user']] and 'password64' not in config[config_to_lower['user']]:
-        return None, InvalidConfigurationFile("password parameter not found")
+        return None, InvalidConfigurationFile('password parameter not found')
 
     return config, None
 
@@ -118,21 +122,21 @@ class InvalidConfigurationFile(Error):
     """Exception for an invalid configuration file"""
 
     def __init__(self, msg=''):
-        super(InvalidConfigurationFile, self).__init__('Invalid Configuration File: {}'.format(msg))
+        super(InvalidConfigurationFile, self).__init__(f'Invalid Configuration File: {msg}')
 
 
 class UnableToCreateFile(Error):
 
     def __init__(self, msg=''):
-        super(UnableToCreateFile, self).__init__('Unable to Create File: {}'.format(msg))
+        super(UnableToCreateFile, self).__init__(f'Unable to Create File: {msg}')
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
 
-    file_name = "config.ini"
+    file_name = DEFAULT_FILE_NAME
     print('Generating default example configuration file ...')
     try:
         save(config=create_new(), file=file_name)
-    except Exception as err:
-        raise UnableToCreateFile(err)
-    print("File created: {}".format(file_name))
+    except Exception as e:
+        raise UnableToCreateFile(e)
+    print(f'File created: {file_name}')
